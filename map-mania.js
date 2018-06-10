bce1705a881cb6498a8ec55113b27cc52b99cfc2
mramirez81 <39684847+mramirez81@users.noreply.github.com>
2018-06-10 03:59:53 -0500

    Add files via upload

---
+++ map-mania.js
@@ -0,0 +1,29 @@
+var gMap;
+
+
+function initMap() {
+    gMap = new google.maps.Map(document.getElementById('myMapID'), {
+        center: {lat: 41.878, lng: 10}, zoom: 3});
+    var marker = new google.maps.Marker({position:{lat:12.432,lng:-43.234}, map:gMap});   
+    var marker2 = new google.maps.Marker({position:{lat:41.7934,lng:-87.7754}, map:gMap});
+    marker2.setIcon('http://maps.google.com/mapfiles/kml/shapes/info-i_maps.png');
+    var infoWindow = new google.maps.InfoWindow({content:'Chicago, Illinois'});
+    marker2.addListener('click', function() {
+        infoWindow.open(gMap, marker2);
+    });
+    google.maps.event.addListener(gMap, 'idle', function(){
+        updateGame()
+    });
+}
+function updateGame() {
+    console.log('function UpdateGame()!');
+    var zoomLevel = gMap.getZoom()
+    var inBounds = false;
+    if (gMap.getBounds().contains({lat:41.7934,lng:-87.7754})) {
+        inBounds = true;
+    }
+    console.log("inBounds:"+inBounds+"zoomlevel:"+zoomLevel);
+}
+function initApp() {
+    console.log("Map Mania Lite - Starting!");
+}
